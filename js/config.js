2bbab24cfed0ee371bdc3b0bdf8258416a6e0341
Punya Ira Anand <punyaianand01@gmail.com>
2024-12-06 23:57:08 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_l1ZW1MQVR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6na5r8t5gfpg4as97lk6jutp8p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_32wnXRom0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '725qelsdltejd3tignd0r6laoh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
